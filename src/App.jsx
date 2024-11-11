d317dcad5cd97cafa140a4a08d1f5a45c303ea26
cardosomartins <paulohcmartins@hotmail.com>
2024-11-10 22:02:49 -0300

    feat: App now gets data from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import turtle from './public/turtle.png'
 import './App.css'
 import TitleComponent from './Components/TitleComponent.jsx'
@@ -7,36 +7,52 @@ import SearchBarComponent from './Components/SearchBarComponent.jsx'
 import YoutubeThumbnailComponent from './Components/YoutubeThumbnailComponent.jsx'
 import YoutubeEmbedComponent from './Components/YoutubeEmbedComponent.jsx'
 
-function App() {
-  const Thumbnails = ["https://i.ytimg.com/vi/ma67yOdMQfs/default.jpg",
-    "https://i.ytimg.com/vi/gWZ8PwO0CkQ/default.jpg", 
-    "https://i.ytimg.com/vi/DaLUxjNlBSI/default.jpg"];
+const url = "http://localhost:3000/items"
 
-  const Titles = ["These Were The All-Time Surfing Moments Of The Year | Best Of 2020",
-  "BIG WAVE SURFING COMPILATION 2024 * TOUR OF DUTY *",
-  "Girl Tries Surfing Simulator ❤️"
+async function RetrieveData(){
+  console.log("Starting Retrieve Data Method");
+  const res = await fetch(url)
+  const data = await res.json();
+  console.log("Retrieve Data method executed! See data above and below");
+  return data;
+}
 
-  ];
 
-  const PublishDates = ["2021-01-23T17:00:15Z", "2024-05-25T00:03:07Z", ];
+function App() {  
+  
+  const handleClick = (
+    () => {
+      setToggle(!toggleState);
+      const Prefix = "https://www.youtube.com/watch?v="
 
-  const Descriptions = ["Well, that was a weird ride. Though it hasn't been easy, at least when we fixed our gaze on the ocean — or favorite place in the ...",
-  "BIG WAVE SURFING COMPILATION 2024 ** TOUR OF DUTY ** ** WORLD RECORD MONSTERS 60-100ft OF PURE FEAR ...",
-  "This girl wanted to surf in the dangerous ocean, so she practiced on a surfing simulator with high tide. Her trainer held her with a ..."]
+      RetrieveData().then(result =>
+        setData(result));    
+    }
+  );
 
+  const [toggleState, setToggle] = useState(false);
+
+  useEffect(() => {}, [toggleState])
+
+  const [data, setData] = useState([])
+  const VideoURLs = []
   const Prefix = "https://www.youtube.com/watch?v="
-  const VideoURLs = ["ma67yOdMQfs", "gWZ8PwO0CkQ", "DaLUxjNlBSI"]
-
 
   return (
     <>
       <TitleComponent/>
       <div> <img src={turtle} alt="turtle" /> </div>
+
       <SearchDescriptionComponent/>
+
       <SearchBarComponent/>
-      {Thumbnails.map((item, index) =>
-      <YoutubeThumbnailComponent Key = {index} Title={Titles[index]} Description={Descriptions[index]} MediumThumbnail={Thumbnails[index]} PublishDate={PublishDates[index]} VideoURL = {Prefix+VideoURLs[index]} />)}
-      {VideoURLs.map((item, index) => <YoutubeEmbedComponent VideoId={VideoURLs[index]} />)}        
+
+      {data.map((item, index) =>
+      <YoutubeThumbnailComponent Key = {index} Title={item.snippet.title} Description={item.snippet.description} MediumThumbnail={item.snippet.thumbnails.default.url} PublishDate={item.snippet.publishedAt} VideoURL = {Prefix + item.id.videoId} />)}
+      
+      {data.map((item, index) => <YoutubeEmbedComponent Key = {index} VideoId={item.id.videoId} />)}
+
+      <button onClick={handleClick}> Click here to retrieve Videos! {toggleState}</button>      
     </>
   )
 }
